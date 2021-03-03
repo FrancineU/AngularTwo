31c9317fbcaa35ad1975a7411df51bf575854f8c
=FrancineU <uwizeyimanafrancine62@gmail.com>
2021-03-03 12:29:36 +0200

    Added components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormComponent } from './form/form.component';
+import { RepositoriesComponent } from './repositories/repositories.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { UserInfoComponent } from './user-info/user-info.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormComponent,
+    RepositoriesComponent,
+    NavbarComponent,
+    UserInfoComponent
   ],
   imports: [
     BrowserModule,
